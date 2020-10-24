63e7132e800c0e8c40b183860d504dc17ae5197e
MAHMOUDASEM <48389287+MAHMOUDASEM@users.noreply.github.com>
2020-10-24 20:59:42 +0900

    Added Web App for website live integration

--- utils.py
+++ utils.py
@@ -41,3 +41,60 @@ def showHeatMap(grid,annotate=False,figures = None,title='',bar=False,figsize=(1
     plt.xticks([]);
     plt.yticks([])
     plt.show() 
+
+
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import ImageGrid
+
+def show_heat_maps(*grids,annotate=False,save=False):
+
+    
+#     if annotate:
+#         N,M = int(grids1.shape[0]),int(grids1.shape[1])
+#         for k in range(N):
+#             for j in range(M):
+#                 text1 = ax1.text(j, k, 
+#                                np.round(grids1[k, j],1),
+#                                ha="center", va="center", color="w",fontsize=12)
+                
+#                 text2 = ax2.text(j, k, 
+#                                np.round(grids2[k, j],1),
+#                                ha="center", va="center", color="w",fontsize=12)
+     
+# #                 text3 = ax3.text(j, k, 
+# #                                np.round(diff[k, j],1),
+# #                                ha="center", va="center", color="w")
+
+    
+    # Set up figure and image grid
+    fig = plt.figure(figsize=(30, 10))
+
+    grid = ImageGrid(fig, 111,          # as in plt.subplot(111)
+                     nrows_ncols=(1,len(grids)),
+                     axes_pad=0.25,
+                     share_all=True,
+                     cbar_location="left",
+                     cbar_mode="single",
+                     cbar_size="5%",
+                     cbar_pad=0.25,
+                     )
+
+    # Add data to image grid
+    for ax,g in zip(grid,grids):#,['Numerical solution','DefusionNet','Difference']):
+        im = ax.imshow(g[0])
+        ax.title.set_text(g[1])
+
+
+        if annotate:
+            N,M = int(g[0].shape[0]),int(g[0].shape[1])
+            for k in range(N):
+                for j in range(M):
+                    text1 = ax.text(j, k, np.round(g[0][k, j],1),ha="center", va="center", color="w",fontsize=12)
+
+    # Colorbar
+    ax.cax.colorbar(im)
+    ax.cax.toggle_label(True)
+
+    #plt.tight_layout()    # Works, but may still require rect paramater to keep colorbar labels visible
+    plt.show()
